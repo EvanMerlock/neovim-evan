95715a3206542fba5e58e0aab7e1932223f4e630
Evan Merlock <evan@merlock.dev>
2026-02-16 17:16:19 -0600

    nit: oops

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Evan's personal nvim config, based on Kickstart.nvim and adjusted to be Nix-friendly";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
       url = "github:nix-community/home-manager";
